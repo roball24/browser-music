7a7e6dce5a71ec3193c899fff373ff6d097ee23c
Robert Allen <robby.allen24@gmail.com>
2017-04-28 23:26:12 -0400

    moved redux store into app.jsx for simplicity

--- frontend-react/src/app.jsx
+++ frontend-react/src/app.jsx
@@ -1,8 +1,19 @@
 import React from 'react'; // eslint-disable-line no-unused-vars
 import { render } from 'react-dom';
+
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import store from './store';
+import thunkMiddleware from 'redux-thunk';
+import reducers from './reducers';
+
 import { Routing } from './containers';
+
+
+// import createLogger from 'redux-logger';
+// const logger = createLogger();
+
+let store = applyMiddleware(thunkMiddleware)(createStore)(reducers);
+window.s = store;
 
 render((
 	<Provider store={store}>
